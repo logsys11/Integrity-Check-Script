e37b870a080b2fb7377d14bd9438a9d07a5cb8e2
logsys11 <logsys11@users.noreply.github.com>
2016-12-15 16:57:48 +0000

    Updated to Version 0.2

--- hashing.py
+++ hashing.py
@@ -1,8 +1,38 @@
 import hashlib
 import sys
 import os
+import platform
 
-clear = lambda: os.system('clear')
+sys.stdout.write("\x1b[8;24;90t")
+
+### OPEN-SOURCE CODE LICENSES ###
+# # Visual Progress Bar License# #
+# The MIT License (MIT)
+# Copyright (c) 2016 Vladimir Ignatev
+#
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software 
+# is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# # END OF PROGRESS BAR LICENSE # #
+### END OF LICENSES ###
+
+if platform.system() == "Windows":
+	clear = lambda: os.system('cls')
+else:
+	clear = lambda: os.system('clear')
 
 BLOCKSIZE = 65536
 ARGLIST = sys.argv
@@ -32,42 +62,66 @@ def WARNING(string):
 def UNDERLINE(string):
 	return bcolors.UNDERLINE + string + bcolors.ENDC
 
+def exitHelp():
+	print("Usage: [option] (filename)")
+	print(" -<option>:")
+	print("  -c - Check file integrity by comparing with existing hash file(Requires filename after)")
+	print("  -g - Generate a hash file to check file integrity later(Requires filename after)")
+	print("  -h - Show this help page")
+	print("")
+	print(" *()* Required for some options")
+	sys.exit()
+
+def progress(count, total, status=''):
+  bar_len = 60
+  filled_len = int(round(bar_len * count / float(total)))
+  percents = round(100.0 * count / float(total), 1)
+  bar = '=' * filled_len + '-' * (bar_len - filled_len)
+  sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+  sys.stdout.flush()
+	#For license check above
+
 #Introduction
 clear()
-print(BOLD("Integrity check v.0.1 By Logsys"))
-print("")
-print("All rights reserved. Copyright 2016\n")
-if len(ARGLIST) > 1:
-	if (ARGLIST[1] == "check"):
-		filename = ARGLIST[2]
-	elif (ARGLIST[1] == "generate"):
-		filename = ARGLIST[2]
+print(BOLD("Integrity check v.0.2 By Logsys"))
+print("\nLicensed under MIT License.\nFor more information check https://github.com/logsys11/Integrity-Check-Script\n\n")
+#print(str(ARGLIST))
+for i in range(0,len(ARGLIST)):
+	if ARGLIST[i] == "-h":
+		exitHelp()
+	elif ARGLIST[i] == "-c":
+		filename = ARGLIST[i+1]
+		break
+	elif ARGLIST[i] == "-g":
 		GENERATE = True
+		filename = ARGLIST[i+1]
+		break
 	else:
-		print(" Usage: <flag> <filename>")
-		print("   -<flag>:")
-		print("     check - Check file integrity by comparing with existing hash file")
-		print("     generate - Generate a hash file to check file integrity later")
-else:
-	print("   Usage: <flag> <filename>")
-	print("-<flag>:")
-	print("  check - Check file integrity by comparing with existing hash file")
-	print("  generate - Generate a hash file to check file integrity later")
-	sys.exit()
-	
+		if i == len(ARGLIST):
+			exitHelp()	
 
 md5hasher = hashlib.md5()
 sha1hasher = hashlib.sha1()
 sha256 = hashlib.sha256()
 
 with open(filename, 'rb') as afile:
+	print("Loading file " + filename)
+	totalbuf = BLOCKSIZE
+	totalsize = os.fstat(afile.fileno()).st_size
+	print("File size is: " + str(round(totalsize/1024,1)) + "KB\n")
 	buf = afile.read(BLOCKSIZE)
+	
 	while len(buf) > 0:
 		md5hasher.update(buf)
 		sha1hasher.update(buf)
 		sha256.update(buf)
+		totalbuf = totalbuf + BLOCKSIZE
+		if totalbuf > totalsize:
+			totalbuf = totalsize
+		progress(totalbuf,totalsize,status=' Reading file...')
 		buf = afile.read(BLOCKSIZE)
 		
+sys.stdout.write('\n\n')
 md5hash = md5hasher.hexdigest()
 sha1hash = sha1hasher.hexdigest()
 _sha256 = sha256.hexdigest()
